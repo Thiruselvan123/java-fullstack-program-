6283d99bfb4590b06014a2a4957220382fc4ca3a
Thiruselvan123 <123719355+Thiruselvan123@users.noreply.github.com>
2024-10-24 16:08:15 +0530

    hi hello

---
+++ core_java_program/src/day_1/Demo.java
@@ -0,0 +1,18 @@
+package day_1;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int N;
+		String N1;
+		Scanner S=new Scanner(System.in);
+		N=S.nextInt();
+		N1=
+		System.out.println("The first Number is:"+N+" and the Second number is:"+N1);
+		
+	}
+		
+
+}
