b2ab319e74f217c5f6e30f79ed1ca2d29836bd75
Thiruselvan123 <123719355+Thiruselvan123@users.noreply.github.com>
2024-10-25 16:07:19 +0530

    Update Demo.java

--- core_java_program/src/day_1/Demo.java
+++ core_java_program/src/day_1/Demo.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Demo {
 
 	public static void main(String[] args) {
-		int N;
-		String N1;
+		int N,N1;
 		Scanner S=new Scanner(System.in);
 		N=S.nextInt();
-		N1=
+		N1=S.nextInt();
 		System.out.println("The first Number is:"+N+" and the Second number is:"+N1);
 		
 	}
